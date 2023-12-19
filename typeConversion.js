782bb158443e7236bf194e57805135543bd7d43a
Ritik Kumar <ritik.kumar@simbaquartz.com>
2023-12-19 10:11:54 +0530

    learn prefix & postfix

--- typeConversion.js
+++ typeConversion.js
@@ -1,7 +1,22 @@
-const num = "hvgghvd";
-const num2 = Boolean(num);
-console.log(typeof num2);
-console.log(num2);
+// const num = "hvgghvd";
+// const num2 = Boolean(num);
+// console.log(typeof num2);
+// console.log(num2);
 
 // true =>1
 // false =>0
+
+// Prefix
+// Postfix
+
+// Postfix
+// let x = 5;
+// let y = x++;
+// console.log("Y", y);
+// console.log("X", x);
+
+// Prefix
+// let x = 5;
+// let y = ++x;
+// console.log("Y", y);
+// console.log("X", x);
