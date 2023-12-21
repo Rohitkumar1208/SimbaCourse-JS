3b24c8be09f2ddbbc1b7b1366ebadff83d79d909
Ritik Kumar <ritik.kumar@simbaquartz.com>
2023-12-21 09:39:55 +0530

    learn about js memory

---
+++ heap&stack.js
@@ -0,0 +1,24 @@
+let name2 = "Hello World";
+let name1 = name2;
+name1 = "Rohit";
+console.log(name1);
+
+// primitive===========================
+// heapMemory;
+// name2;
+
+// non-premitive==========
+
+// const obj = {
+//   name: "Ritik",
+//   agea: 10,
+//   address: "Amritsar",
+//   phn: 87654345678,
+// };
+
+// let obj2 = obj;
+// obj2.name = "Rohit";
+// console.log(obj2);
+// console.log(obj);
+
+// Stack Memory
